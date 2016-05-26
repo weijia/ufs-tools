144f53428196a68153ae36a23fbb803fc79c78de
richard <richardwangwang@gmail.com>
2016-05-26 10:37:32 +0800

    Fix remove_duplicated_keep_order.

--- ufs_tools/tuple_tools.py
+++ ufs_tools/tuple_tools.py
@@ -1,8 +1,8 @@
 
-def remove_duplicated(value_in_tuple):
-    # new_tuple = []
-    # for i in value_in_tuple:
-    #     if not (i in new_tuple):
-    #         new_tuple.append(i)
-    # return new_tuple
-    return tuple(set(value_in_tuple))
+def remove_duplicated_keep_order(value_in_tuple):
+    new_tuple = []
+    for i in value_in_tuple:
+        if not (i in new_tuple):
+            new_tuple.append(i)
+    return new_tuple
+    # return tuple(set(value_in_tuple))
