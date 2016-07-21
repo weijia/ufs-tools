359aeced1dadd3376c3056def3d213eac9279516
richard <richardwangwang@gmail.com>
2016-07-21 19:09:24 +0800

    Update app framework.

--- ufs_tools/app_framework.py
+++ ufs_tools/app_framework.py
@@ -7,17 +7,20 @@ from folder_tool import get_root_sub_folder, ensure_dir
 
 
 class AppConfig(object):
-    def __init__(self, file_path, root_name):
-        self.file_path = file_path
-        self.root_name = root_name
+    def __init__(self, root_folder):
+        # self.file_path = file_path
+        # self.root_name = root_name
+        self.root_folder = root_folder
 
     def get_or_create_app_data_folder(self, data_folder_name):
-        app_data_folder = get_root_sub_folder(self.file_path, self.root_name, "../data/%s" % data_folder_name)
+        # app_data_folder = get_root_sub_folder(self.file_path, self.root_name, "../data/%s" % data_folder_name)
+        app_data_folder = os.path.join(self.root_folder, "../data/%s" % data_folder_name)
         ensure_dir(app_data_folder)
         return app_data_folder
 
     def get_app_in_framework(self, folder, name):
-        other_app_full_path = get_root_sub_folder(self.file_path, self.root_name, "../others/%s" % folder)
+        # other_app_full_path = get_root_sub_folder(self.file_path, self.root_name, "../others/%s" % folder)
+        other_app_full_path = os.path.join(self.root_folder, "../others/%s" % folder)
         full_path = os.path.join(other_app_full_path, name)
         if os.path.exists(full_path):
             return full_path
